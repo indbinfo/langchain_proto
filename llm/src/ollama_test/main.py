2c1a5697f36db9769dd0558aaff0f25548deb815
indbinfo <openweb@indbinfo.com>
2024-03-27 14:06:49 +0900

    테이블 정렬, 실행중 ui 추가

--- llm/src/ollama_test/main.py
+++ llm/src/ollama_test/main.py
@@ -81,28 +81,29 @@ def main_ui():
             """):
             if st.form_submit_button(label='검색하기'):
                 if user_question:
-                    response = get_response(code_return, user_question, 'wizardcoder:34b-python')
-                    st.image(root_dir + 'result/corp_rate.png', use_column_width=True)
-                    st.write_stream(response)
+                    with st.spinner('실행중입니다..'):
+                        response = get_response(code_return, user_question, 'wizardcoder:34b-python')
+                        st.image(root_dir + 'result/corp_rate.png', use_column_width=True)
+                        st.write_stream(response)
 
-                    st.markdown("""<table class="info-table">
-                            <tr>
-                                <th>제공기관</th>
-                                <th>설명</th>
-                                <th>가격</th>
-                                <th>유관상품</th>
-                            </tr>
-                            <tr>
-                                <td>비씨카드</td>
-                                <td>내국인 카드소비데이터</td>
-                                <td>3,000,000원</td>
-                                <td><a href="https://www.bigdata-finance.kr/dataset/datasetView.do?datastId=SET0300009" style="font-weight: bold;">바로가기</a></td>
-                            </tr>
-                            </table><br>
-                    """, unsafe_allow_html=True
-                    )
-                    words = ["Hello,", "this", "is", "a", "simulation", "of", "ChatGPT", "typing."]
-                    simulate_typing_effect(words, typing_speed=0.1)
+                        st.markdown("""<table class="info-table" style="margin-left: auto; margin-right: auto;">
+                                <tr>
+                                    <th>제공기관</th>
+                                    <th>설명</th>
+                                    <th>가격</th>
+                                    <th>유관상품</th>
+                                </tr>
+                                <tr>
+                                    <td>비씨카드</td>
+                                    <td>내국인 카드소비데이터</td>
+                                    <td>3,000,000원</td>
+                                    <td><a href="https://www.bigdata-finance.kr/dataset/datasetView.do?datastId=SET0300009" style="font-weight: bold;">바로가기</a></td>
+                                </tr>
+                                </table><br>
+                        """, unsafe_allow_html=True
+                        )
+                        words = ["Hello,", "this", "is", "a", "simulation", "of", "ChatGPT", "typing."]
+                        simulate_typing_effect(words, typing_speed=0.1)
 
 if __name__ == "__main__":
     main_ui()

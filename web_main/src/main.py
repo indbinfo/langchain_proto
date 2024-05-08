769f1d6a29efc7dcb64aa130b24f6dc871adcf54
Ys Son <ysson96@gmail.com>
2024-05-08 10:59:58 +0900

    pylint, errorlens 기반 코드 수정 - 모듈 상단에 상수 정의, 대문자로 변경 - 환경에 따라 경로 설정하는 코드로 변경 - 띄어쓰기, 공백, 들여쓰기 변경/추가 - 모듈 및 함수 수준 Docstring 추가 - built-in 매개변수명 변경 - 사용하지 않은 모듈 import 제외 - %s 포맷팅으로 변경

--- web_main/src/main.py
+++ web_main/src/main.py
@@ -1,54 +1,96 @@
+"""
+* Version: 1.0
+* 파일명: main.py
+* 설명: 사용자 프롬프트를 입력 받아 코드를 생성하고 실행한 후 결과를 반환하는 모듈
+* 수정일자: 2024/05/07
+* 수정자: 손예선
+* 수정 내용
+    1. with open 뒤 띄어쓰기 삭제
+    2. 환경에 따라 경로 설정
+    3. "if valid_yn == False" -> "if valid_yn is False"
+"""
 import json
 import os
-from datetime import datetime
-from langchain_core.runnables import RunnablePassthrough, RunnableParallel
-from langchain_core.output_parsers import StrOutputParser
-from preprocess.load_data import load_template
-from model.load_model import OllamaModelLoader
-from preprocess.qdrant import VectorDB, get_filter, format_docs, remove_overlaps_in_sequence
-from preprocess.save_file import save_execute_python
-# from custom_log import setup_logger
 import logging
 import logging.config
 
-config_path = "/home/prompt_eng/langchain/langchain_proto/web_main/config/config.json"
+from langchain_core.output_parsers import StrOutputParser
+from model.load_model import OllamaModelLoader
+from preprocess.load_data import load_template
+from preprocess.qdrant import VectorDB, get_filter
+from preprocess.save_file import save_execute_python
 
-with open('/home/prompt_eng/langchain/langchain_proto/web_main/config/logging.json','r') as f:
+# 운영 체제에 따라 Config 경로 설정
+if os.name == 'posix':
+    CONFIG_PATH = os.path.join(
+        os.environ.get('HOME'),
+        'langchain_proto',
+        'web_main',
+        'config',
+    )
+elif os.name == 'nt':
+    CONFIG_PATH = os.path.join(
+       'C:', 
+       os.environ.get('HOMEPATH'),
+       "langchain_proto", 
+       "web_main", 
+       "config",
+    )
+else:
+    CONFIG_PATH = None
+
+with open(os.path.join(CONFIG_PATH, 'logging.json'),'r', encoding='utf-8') as f:
     lc = json.load(f)
     logging.config.dictConfig(lc)
 logger = logging.getLogger("main")
 
-# file_idx = datetime.now().strftime("%M")
-file_idx = 1
 
-with open (config_path) as f:
+FILE_IDX = 1
+with open(os.path.join(CONFIG_PATH, 'config.json'), 'r', encoding='utf-8') as f:
     config = json.load(f)
-# csv_dir = os.path.join(config["path"]["src_path"])
-graph_dir = os.path.join(config["path"]["root_path"],config["path"]["result_path"],config["path"]["graph_path"])
-save_dir = os.path.join(config["path"]["root_path"],config["path"]["result_path"])
-# code_file = config["path"]["code_file"].format(file_idx)
-report_dir = os.path.join(config["path"]["root_path"],config["path"]["result_path"],config["path"]["report_path"])
-# report_file = config["path"]["report_file"].format(file_idx)
-# graph_file = config["path"]["graph_file"].format(file_idx)
-# code_path = os.path.join(save_dir,code_file)
 
-# graph_path = os.path.join(graph_dir, graph_file)
-# report_path = os.path.join(report_dir, report_file)
+graph_dir = os.path.join(
+    config["path"]["root_path"],
+    config["path"]["result_path"],
+    config["path"]["graph_path"]
+    )
 
-# logger_path = config["path"]["logger_path"]
-# logger = setup_logger(logger_path,'main.log')
+save_dir = os.path.join(
+    config["path"]["root_path"],
+    config["path"]["result_path"]
+    )
+
+report_dir = os.path.join(
+    config["path"]["root_path"],
+    config["path"]["result_path"],
+    config["path"]["report_path"]
+    )
 
 def response_from_llm(user_prompt, file_idx = 1):
+    """
+    유저 프롬프트를 입력 받아 코드를 생성하고 실행한 후 결과를 반환
+
+    Args:
+        user_prompt (str): 사용자가 입력한 프롬프트
+        file_idx (int, optional): 결과를 저장할 파일의 인덱스. default=1
+    
+    Returns:
+        tuple: 코드 실행 결과, 그래프 파일 경로, 리포트 파일 경로로 구성된 튜플 반환
+    """
     code_file = config["path"]["code_file"].format(file_idx)
     code_path = os.path.join(save_dir,code_file)
     report_file = config["path"]["report_file"].format(file_idx)
     report_path = os.path.join(report_dir, report_file)
     graph_file = config["path"]["graph_file"].format(file_idx)
     graph_path = os.path.join(graph_dir, graph_file)
-    #user_prompt = "독산동의 법인카드 매출을 시간대 별로 알려줘"
+
     # template 로드
     code_gen_input = ["context","report_file","graph_file","task"]
-    code_gen_temp = load_template(config['template_file']['code-gen'], input_variables=code_gen_input)
+    code_gen_temp = load_template(
+        config['template_file']['code-gen'],
+        input_variables=code_gen_input
+    )
+
     # 모델 로드
     code_gen_loader = OllamaModelLoader(model_id = "wizardcoder:34b-python", pt_task = "code-gen")
     code_gen_mdl = code_gen_loader.load_model()
@@ -58,73 +100,71 @@ def response_from_llm(user_prompt, file_idx = 1):
     qd_client = qdrant.create_connection()
 
     # 유/무효 질문 검증
-    result = qdrant.qdrant_similarity_search(client = qd_client,
-                            task = user_prompt,
-                            collection_name= "question", 
-                            filter=None,
-                            k=5)
+    result = qdrant.qdrant_similarity_search(
+        client = qd_client,
+        task = user_prompt,
+        collection_name = "question",
+        filters=None,
+        k=5
+    )
 
+    # 기본값 설정
     return_txt = "요청 주신 질문에 대해서 답변이 어렵습니다."
     valid_yn = True
+
     for data in result:
         valid_filter = data[0].metadata['filter']
         logger.info(valid_filter)
         print(valid_filter)
+
         if valid_filter == "무효질문":
             print(data[0].page_content)
             valid_yn = False
             break
-    if valid_yn == False:
+
+    if valid_yn is False:
         return return_txt, graph_path, report_path
 
-    prompt_query_doc = qdrant.qdrant_similarity_search(client=qd_client,
-								task=user_prompt,
-								collection_name="context",
-								k=1)
-    #logger.info(f"prompt_query:\n{prompt_query_doc}")
+    prompt_query_doc = qdrant.qdrant_similarity_search(
+        client=qd_client,
+		task=user_prompt,
+		collection_name="context",
+	    k=1
+    )
+
     prompt_no = prompt_query_doc[0][0].metadata['filter']
-    # logger.info(f"prompt_no:{prompt_no}")
-    data = qdrant.qdrant_similarity_search(client=qd_client,
-								task=user_prompt,
-								collection_name="document",
-								filter=get_filter("filter",prompt_no),
-								k=1)
-    logger.info(f"프롬프트 번호:{prompt_no}")
+
+    data = qdrant.qdrant_similarity_search(
+        client=qd_client,
+		task=user_prompt,
+		collection_name="document",
+		filters=get_filter("filter", prompt_no),
+		k=1,
+    )
+
+    logger.info("프롬프트 번호: %s", prompt_no)
     context = data[0][0].page_content
-    # 경로 받기
+
+    # 경로 받아오기
     partial_prompt = code_gen_temp.partial(
                                         graph_file=graph_file,
                                         report_file=report_file,
                                         context = context
                                         )
-    # qd_obj = qdrant.create_Qdrant_obj(client=qd_client, collection_name= 'format')
-    # retriever = qd_obj.as_retriever(filter = get_filter("filter","format"))
-    # setup_and_retrieval = RunnableParallel(
-    #     {"context": retriever| format_docs, "task": RunnablePassthrough()})
-
-    # chain = setup_and_retrieval | partial_prompt | code_gen_mdl | StrOutputParser()
     chain = partial_prompt | code_gen_mdl | StrOutputParser()
     code_txt = chain.invoke({"task":user_prompt})
-    logger.info(f"코드 결과:\n{code_txt}")
+    logger.info("코드 결과:\n%s", code_txt)
     prompt_result = partial_prompt.format(task=user_prompt)
-    logger.info(f"프롬프트 결과:\n{prompt_result}")
+    logger.info("프롬프트 결과:\n%s", prompt_result)
 
     return_code = save_execute_python(code_txt,code_path)
     if return_code == 0:
         return_txt = "모델이 성공적으로 결과를 생성하였습니다."
     elif return_code == 1:
         return_txt = "파일 실행에 실패하였습니다."
-    
-    
-
-    
 
     return return_txt, graph_path, report_path
 
 
 if __name__ == "__main__":
     response_from_llm(user_prompt="독산동의 법인카드 매출을 시간대 별로 알려줘")
-# 독산동의 법인카드 매출을 시간대 별로 알려줘
-
-
-
--- web_main/src/preprocess/load_data.py
+++ web_main/src/preprocess/load_data.py
@@ -1,14 +1,45 @@
-from langchain.prompts.prompt import PromptTemplate
+"""
+* Version: 1.0
+* 파일명: load_data.py
+* 설명: 기존 템플릿 파일을 가져오는 모듈
+* 수정일자: 2024/05/02
+* 수정자: 손예선
+* 수정 내용
+    1. 환경에 따라 경로 설정
+"""
+
 import os
+from langchain.prompts.prompt import PromptTemplate
 
-template_path = "/home/prompt_eng/langchain/langchain_proto/web_main/data/template"
-
-
+if os.name == 'posix':
+    PATH = os.path.join(
+        os.environ.get('HOME'),
+        'langchain_proto',
+        'web_main',
+    )
+elif os.name == 'nt':
+    PATH = os.path.join(
+        'c:', 
+        os.environ.get('HOMEPATH'),
+        'langchain_proto',
+        'web_main',
+    )
+else:
+    PATH = None
 
 def load_template(template_file, input_variables):
-    with open(os.path.join(template_path,template_file)) as f:
+    """
+    기존 파일에서 Template 로드
+
+    Args:
+        template_file (str): 템플릿 파일명
+        input_variables (dict): 템플릿에서 사용되는 변수들
+
+    Returns: 
+        기존 파일에서 로드한 Prompt Template
+    """
+    with open(os.path.join(PATH, 'data', 'template', template_file), encoding='utf-8') as f:
         template = f.read()
     prompt = PromptTemplate(template=template, input_variables=input_variables)
 
     return prompt
-

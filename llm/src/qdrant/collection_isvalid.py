3466f37ec786fc6a54e1773b8ab6a642c6fe3a83
east-right <wkfl1125@naver.com>
2024-05-08 10:36:44 +0900

    flake9내용만수정

--- llm/src/qdrant/collection_isvalid.py
+++ llm/src/qdrant/collection_isvalid.py
@@ -6,7 +6,9 @@ import numpy as np
 import json
 
 # config
-with open('/home/llm/langchain_proto/web_main/config/qdrant.json', 'r', encoding='utf8') as f:
+with open('/home/llm/langchain_proto/web_main/config/qdrant.json',
+          'r',
+          encoding='utf8') as f:
     config = json.load(f)
 
 QDRANT_HOST = config['QDRANT_HOST']
@@ -17,12 +19,13 @@ client = QdrantClient(
     api_key=QDRANT_API_KEY
 )
 
+
 class VectorDB:
     def __init__(self, client, collection_name, size=768):
         self.client = client
         self.collection_name = collection_name
-        self.size=size
-    
+        self.size = size
+
     # get embedding model
     def get_embedding(self):
         embeddings = HuggingFaceEmbeddings(
@@ -31,7 +34,7 @@ class VectorDB:
             encode_kwargs={'normalize_embeddings': True}
         )
         return embeddings
-    
+
     # create collection
     def create_collection(self):
         collection_config = qdrant_client.http.models.VectorParams(
@@ -40,21 +43,19 @@ class VectorDB:
         )
 
         client.recreate_collection(
-            collection_name = self.collection_name,
-            vectors_config = collection_config
+            collection_name=self.collection_name,
+            vectors_config=collection_config
         )
 
     def add_vectorstore(self, text_chunks, ids, metadatas):
         vectorstore = Qdrant(
-            client = self.client,
-            collection_name = self.collection_name,
-            embeddings = self.get_embedding()
+            client=self.client,
+            collection_name=self.collection_name,
+            embeddings=self.get_embedding()
         )
-        vectorstore.add_texts(text_chunks,
-                             ids=ids,
-                             metadatas=metadatas
-                             )
+        vectorstore.add_texts(text_chunks, ids=ids, metadatas=metadatas)
         return vectorstore
+
 
 if __name__ == "__main__":
     collection_name = 'question'
@@ -63,31 +64,33 @@ if __name__ == "__main__":
     vectordb.create_collection()
 
     # 유·무효질문
-    with open('/home/llm/langchain_proto/vec/questions.json', 'r', encoding='utf8') as f:
+    with open('/home/llm/langchain_proto/vec/questions.json',
+              'r',
+              encoding='utf8') as f:
         questions = json.load(f)
 
-    meta_1=list(np.repeat({"filter":"무효질문"},len(questions['무효질문'])))
-    meta_2=list(np.repeat({"filter":"유효질문"},len(questions['유효질문'])))
+    meta_1 = list(np.repeat({"filter": "무효질문"}, len(questions['무효질문'])))
+    meta_2 = list(np.repeat({"filter": "유효질문"}, len(questions['유효질문'])))
 
     question_list = questions['무효질문'] + questions['유효질문']
     ids = list(range(len(meta_1) + len(meta_2)))
 
     # vectorstore 저장
     vec = vectordb.add_vectorstore(
-        text_chunks=question_list, 
-        ids=ids, 
-        metadatas=sum([meta_1,meta_2] , [])
+        text_chunks=question_list,
+        ids=ids,
+        metadatas=sum([meta_1, meta_2], [])
         )
 
     # payload 설정
     client.set_payload(
         collection_name=collection_name,
         payload={'filter': '무효질문'},
-        points=list(range(0,39))
+        points=list(range(0, 39))
     )
 
     client.set_payload(
         collection_name=collection_name,
-        payload={'filter' : '유효질문'},
-        points=list(range(39,94))
+        payload={'filter': '유효질문'},
+        points=list(range(39, 94))
     )
--- llm/src/qdrant/similarity_search.py
+++ llm/src/qdrant/similarity_search.py
@@ -18,6 +18,7 @@ client = QdrantClient(
     api_key=QDRANT_API_KEY
 )
 
+
 class QAResponse:
     def __init__(self, model_id, collection_name, client, task):
         self.model_id = model_id
@@ -28,11 +29,11 @@ class QAResponse:
     def count(self):
         client = self.client
         cnt = client.count(
-            collection_name = self.collection_name,
-            exact = True,
+            collection_name=self.collection_name,
+            exact=True,
         )
         return cnt
-    
+
     def get_embedding(self):
         embeddings = HuggingFaceEmbeddings(
             model_name="jhgan/ko-sroberta-multitask",
@@ -40,36 +41,35 @@ class QAResponse:
             encode_kwargs={'normalize_embeddings': True}
         )
         return embeddings
-    
+
     def load_llm(self):
         return Ollama(model=self.model_id)
-    
+
     def get_filter(self, key, value):
-        filter = models.Filter(
-        must=[models.FieldCondition(
-            key=key, 
-            match = models.MatchValue(value=value))
+        filter = models.Filter(must=[models.FieldCondition(
+            key=key,
+            match=models.MatchValue(value=value))
             ]
         )
         return filter
-    
+
     def qdrant_qa_response(self):
         vectordb = Qdrant(
-            client = self.client,
-            collection_name = self.collection_name,
-            embeddings = self.get_embedding()
+            client=self.client,
+            collection_name=self.collection_name,
+            embeddings=self.get_embedding()
         )
         llm = self.load_llm()
         qa = RetrievalQA.from_chain_type(
-            llm = llm,
+            llm=llm,
             chain_type='stuff',
             retriever=vectordb.as_retriever(),
             return_source_documents=True,
         )
 
-        response = qa.invoke({'query' : self.task})
+        response = qa.invoke({'query': self.task})
         return response
-    
+
     def isValid(self, result):
         valid_yn = True
         for data in result:
@@ -77,38 +77,39 @@ class QAResponse:
             if valid_filter == "무효질문":
                 valid_yn = False
                 break
-        if valid_yn == False:
+        if valid_yn is False:
             return "요청 주신 질문에 대해서 답변이 어렵습니다."
         else:
             return "답변 제공"
-    
+
     def qdrant_similarity_search(self, k, score_threshold, filter):
         vectordb = Qdrant(
             client=self.client,
             collection_name=self.collection_name,
             embeddings=self.get_embedding()
             )
-        
+
         search_result = vectordb.similarity_search_with_score(
-            query=self.task, 
+            query=self.task,
             k=k,
-            score_threshold =score_threshold, 
+            score_threshold=score_threshold,
             filter=filter
             )
-        
+
         return search_result
 
+
 config = {
-    'model_id' : 'wizardcoder:34b-python',
-    'collection_name' : 'context_ys',
-    'client' : client,
-    'task' : sys.argv[1]
+    'model_id': 'wizardcoder:34b-python',
+    'collection_name': 'context_ys',
+    'client': client,
+    'task': sys.argv[1]
     }
 
 qa_response = QAResponse(**config)
 
 result_k1 = qa_response.qdrant_similarity_search(
-    k=1, 
+    k=1,
     score_threshold=0,
     filter=None
 )
@@ -121,7 +122,7 @@ search_result = qa_response.qdrant_similarity_search(
     score_threshold=0,
     k=100,
     filter=qa_response.get_filter(
-        key='filter', 
+        key='filter',
         value=filter_value
         ),
     )

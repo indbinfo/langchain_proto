daa5e8c60ceece16c896589e504a9d57d990c22c
astride1717 <astride1717@gmail.com>
2024-05-08 10:45:13 +0900

    표준코드관련수정_20240508

--- web_main/src/preprocess/qdrant.py
+++ web_main/src/preprocess/qdrant.py
@@ -1,22 +1,21 @@
+import json
 from qdrant_client import QdrantClient
 import qdrant_client
 from qdrant_client.http import models
-from langchain_community.vectorstores import Qdrant
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from sentence_transformers import SentenceTransformer
-
-import os
-import numpy as np
-import openai
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Qdrant
 import tiktoken
-import json
 
-with open('/home/prompt_eng/langchain/langchain_proto/web_main/config/qdrant.json', 'r', encoding='utf8') as f:
+with open(
+    "/home/prompt_eng/langchain/langchain_proto/web_main/config/qdrant.json",
+    "r",
+    encoding="utf8",
+) as f:
     config = json.load(f)
 
-QDRANT_HOST = config['QDRANT_HOST']
-QDRANT_API_KEY = config['QDRANT_API_KEY']
+QDRANT_HOST = config["QDRANT_HOST"]
+QDRANT_API_KEY = config["QDRANT_API_KEY"]
 
 
 class VectorDB:
@@ -24,101 +23,186 @@ class VectorDB:
         self.host = QDRANT_HOST
         self.api_key = QDRANT_API_KEY
         self.embeddings = HuggingFaceEmbeddings(
-                                    model_name="jhgan/ko-sroberta-multitask",
-                                    model_kwargs={'device': 'cpu'},
-                                    encode_kwargs={'normalize_embeddings': True}
-                                    )
+            model_name="jhgan/ko-sroberta-multitask",
+            model_kwargs={"device": "cpu"},
+            encode_kwargs={"normalize_embeddings": True},
+        )
+
     def create_connection(self):
+        """
+        QdrantClient와의 연결을 생성합니다.
+
+        Returns:
+            QdrantClient: 호스트와 API 키를 사용하여 생성된 QdrantClient의 인스턴스입니다.
+
+        Note:
+            이 함수는 QdrantClient와의 연결을 설정하는 데 사용됩니다. QdrantClient는 호스트와 API 키를 기반으로 데이터를 관리하고 처리합니다.
+        """
         client = QdrantClient(self.host, api_key=self.api_key)
         return client
-    def create_collection(self,qd_client, collection_name):
+
+    def create_collection(self, qd_client, collection_name):
+        """
+        QdrantClient를 사용하여 새로운 컬렉션을 생성합니다.
+
+        Args:
+            qd_client (QdrantClient): QdrantClient의 인스턴스
+            collection_name (str): 생성할 컬렉션의 이름
+
+        Note:
+            이 함수는 QdrantClient를 사용하여 새로운 컬렉션을 생성합니다. 새로운 컬렉션의 구성은 기본값으로 설정되며,
+            크기는 768이며, 거리 메트릭은 코사인 거리로 설정됩니다.
+
+        Warning:
+            이 함수는 기존에 같은 이름의 컬렉션이 있으면 해당 컬렉션을 덮어쓰게 됩니다.
+            따라서 컬렉션을 생성하기 전에 기존 컬렉션이 있는지 확인해야 합니다.
+        """
         collection_config = qdrant_client.http.models.VectorParams(
-        size=768, # 1536 for OpenAI #768 for Gemini/HuggingFace/instructor-xl
-        distance=qdrant_client.http.models.Distance.COSINE)
+            size=768,  # 1536 for OpenAI #768 for Gemini/HuggingFace/instructor-xl
+            distance=qdrant_client.http.models.Distance.COSINE,
+        )
         qd_client.recreate_collection(
-                                   collection_name=collection_name,
-                                   vectors_config=collection_config
-                                  )
+            collection_name=collection_name, vectors_config=collection_config
+        )
 
+    def create_qdrant_obj(self, client, collection_name):
+        """
+        Qdrant 객체를 생성합니다.
 
-    def create_Qdrant_obj(self,client, collection_name):
-        qdrant_obj = Qdrant(client=client,
-                    collection_name=collection_name,
-                    embeddings=self.embeddings
-                    )
+        Args:
+            client (QdrantClient): Qdrant와 통신하기 위한 클라이언트 객체
+            collection_name (str): Qdrant에 저장될 컬렉션의 이름
+
+        Returns:
+            Qdrant: Qdrant 객체로, 클라이언트와 컬렉션 이름, 그리고 임베딩 데이터를 가지고 있습니다.
+
+        Note:
+            이 함수는 주어진 클라이언트와 컬렉션 이름을 사용하여 새로운 Qdrant 객체를 생성합니다.
+            생성된 Qdrant 객체는 클라이언트와 통신할 때 사용되며, 지정된 컬렉션에 임베딩 데이터를 저장하거나 쿼리를 실행합니다.
+        """
+        qdrant_obj = Qdrant(
+            client=client, collection_name=collection_name, embeddings=self.embeddings
+        )
         return qdrant_obj
-    
+
     def tiktoken_len(self, text):
+        """
+        주어진 텍스트의 토큰 길이를 반환합니다.
+
+        Args:
+            text (str): 토큰 길이를 계산할 텍스트
+
+        Returns:
+            int: 텍스트의 토큰 길이
+
+        Note:
+            이 함수는 주어진 텍스트를 처리하여 토큰 길이를 반환합니다. 토크나이저는 'cl100k_base'를 사용합니다.
+            토큰 길이는 텍스트를 인코딩할 때 생성되는 토큰의 개수를 나타냅니다.
+        """
         tokenizer = tiktoken.get_encoding("cl100k_base")
         tokens = tokenizer.encode(text)
         return len(tokens)
-    def get_chunks_test(self, text,chunk_size,chunk_overlap):
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
-            length_function=self.tiktoken_len,
-            add_start_index=True
-        )
-        chunks = text_splitter.split_text(text)
-        return chunks
+
     def get_chunks(self, text):
+        """
+        주어진 텍스트를 청크로 분할하여 반환합니다.
+
+        Args:
+            text (str): 분할할 텍스트
+
+        Returns:
+            list: 텍스트를 분할한 청크들의 리스트
+
+        Note:
+            이 함수는 주어진 텍스트를 주어진 설정에 따라 청크로 분할합니다.
+            청크 크기는 400이며, 청크 간의 중첩은 50입니다.
+            청크를 분할하는 데 사용되는 길이 함수는 'self.tiktoken_len'으로 지정되어 있습니다.
+            각 청크에는 시작 인덱스가 추가됩니다.
+        """
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=400,
             chunk_overlap=50,
             length_function=self.tiktoken_len,
-            add_start_index=True
+            add_start_index=True,
         )
         chunks = text_splitter.split_text(text)
         return chunks
-    def add_vectorstore(self, client, text_chunks, collection_name,ids, metadatas):
-    
+
+    def add_vectorstore(self, client, text_chunks, collection_name, ids, metadatas):
+        """
+        텍스트 청크를 벡터 저장소에 추가합니다.
+
+        Args:
+            client (QdrantClient): 벡터 저장소와 통신하는 데 사용되는 클라이언트 객체
+            text_chunks (list): 텍스트 청크들의 리스트
+            collection_name (str): 텍스트를 저장할 컬렉션의 이름
+            ids (list): 텍스트에 대한 고유 식별자(ID)들의 리스트
+            metadatas (list): 텍스트에 대한 메타데이터들의 리스트
+
+        Note:
+            이 함수는 주어진 클라이언트를 사용하여 텍스트 청크들을 지정된 컬렉션에 추가합니다.
+            각 텍스트 청크는 해당하는 고유 식별자(ID)와 메타데이터와 함께 저장됩니다.
+        """
         # metadatas = list(np.repeat(payload_dict,len(text_chunks)))
-        vectordb = self.create_Qdrant_obj(client, collection_name)
-        vectordb.add_texts(
-                            text_chunks,
-                            ids=ids,
-                            metadatas=metadatas
-                          )
+        vectordb = self.create_qdrant_obj(client, collection_name)
+        vectordb.add_texts(text_chunks, ids=ids, metadatas=metadatas)
         # client.set_payload(
         #                     collection_name=collection_name,
         #                     payload=payload_dict,
         #                     points=ids
         #                   )
-    #def add_vectorstore(self, client, text_chunks, collection_name,ids, metadatas,payload_dict):
+
+    # def add_vectorstore(self, client, text_chunks, collection_name,ids, metadatas,payload_dict):
     # Similarity search with score by filter
     def qdrant_similarity_search(self, client, task, collection_name, filter=None, k=1):
-        vectordb = self.create_Qdrant_obj(client, collection_name)
-        search_result = vectordb.similarity_search_with_score(query=task 
-                                            , k=k       
-                                            , filter=filter)
+        """
+        Qdrant에서 유사도 검색을 수행하고 결과를 반환합니다.
+
+        Args:
+            client (QdrantClient): Qdrant와 통신하는 데 사용되는 클라이언트 객체입니다.
+            task (str): 유사도를 검색할 쿼리나 작업입니다.
+            collection_name (str): 검색을 수행할 컬렉션의 이름입니다.
+            filter (str, optional): 검색 결과를 필터링하는 데 사용할 필터링 조건입니다. 기본값은 None입니다.
+            k (int, optional): 검색 결과로 반환할 최대 항목 수입니다. 기본값은 1입니다.
+
+        Returns:
+            dict: 유사도 검색 결과를 나타내는 사전입니다. 키는 검색된 벡터의 ID이고, 값은 해당 벡터와의 유사도입니다.
+
+        Note:
+            이 함수는 주어진 클라이언트와 컬렉션 이름을 사용하여 Qdrant에서 유사도 검색을 수행합니다.
+            검색 결과는 검색된 벡터의 ID와 해당 벡터와의 유사도를 포함하는 사전 형태로 반환됩니다.
+        """
+        vectordb = self.create_qdrant_obj(client, collection_name)
+        search_result = vectordb.similarity_search_with_score(
+            query=task, k=k, filter=filter
+        )
         return search_result
 
 
-    
-    def create_retriever(self,qd_obj, collection_name, filter_dict):
-        retriever = qd_obj.as_retriever(
-            search_kwargs=dict(
-                filter=qdrant_models.Filter(
-                    must=[
-                        qdrant_models.FieldCondition(
-                            key=key,
-                            match=qdrant_models.MatchValue(value=value)
-                        )
-                    ]
-                )
-            )
-        )
-
 def get_filter(key, value):
-    filter = models.Filter(
-    must=[models.FieldCondition(key=key
-                                , match=models.MatchValue(value=value))]
-                                )
-    return filter
+    """
+    주어진 키와 값으로 필터를 생성하여 반환합니다.
+
+    Args:
+        key (str): 필터링할 필드의 키입니다.
+        value (str): 필터링할 값입니다.
+
+    Returns:
+        Filter: 주어진 키와 값으로 생성된 필터 객체입니다.
+
+    Note:
+        이 함수는 주어진 키와 값으로 필터를 생성합니다. 생성된 필터는 주어진 키와 값에 해당하는 필드의 조건을 나타냅니다.
+    """
+    qdrant_filter = models.Filter(
+        must=[models.FieldCondition(key=key, match=models.MatchValue(value=value))]
+    )
+    return qdrant_filter
+
 
 def format_docs(docs):
-	# return docs[0].page_content
+    # return docs[0].page_content
     return " ".join([doc.page_content for doc in docs])
+
 
 def remove_overlaps_in_sequence(texts):
     """
--- web_main/src/server_http.py
+++ web_main/src/server_http.py
@@ -3,45 +3,47 @@ import base64
 import json
 import os
 import time
-import streamlit as st
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_community.llms import Ollama
-from streamlit_extras.stylable_container import stylable_container
-from main import response_from_llm
-from preprocess.save_file import save_execute_python
 import logging
 import asyncio
+import streamlit as st
+from streamlit_extras.stylable_container import stylable_container
+from main import response_from_llm
 import nest_asyncio
-from custom_log import setup_logger
 
 nest_asyncio.apply()
 # logger = logging.getLogger("main")
 
-config_path = "/home/prompt_eng/langchain/langchain_proto/web_main/config/config.json"
-with open(config_path) as f:
+CONFIG_PATH = "/home/prompt_eng/langchain/langchain_proto/web_main/config/config.json"
+with open(CONFIG_PATH, encoding="utf-8") as f:
     config = json.load(f)
-with open('/home/prompt_eng/langchain/langchain_proto/web_main/config/logging.json','r') as f:
+with open(
+    "/home/prompt_eng/langchain/langchain_proto/web_main/config/logging.json",
+    "r",
+    encoding="utf-8",
+) as f:
     lc = json.load(f)
     logging.config.dictConfig(lc)
 logger = logging.getLogger("main")
 file_idx = datetime.now().strftime("%m%d_%H:%M")
-root_path = config['path']['root_path']
-save_dir = os.path.join(config["path"]["root_path"],config["path"]["result_path"])
+root_path = config["path"]["root_path"]
+save_dir = os.path.join(config["path"]["root_path"], config["path"]["result_path"])
 # code_file = config["path"]["code_file"].format(file_idx)
 # code_path = os.path.join(save_dir,config["path"]["code_path"],code_file)
-logo_file = os.path.join(root_path, 'bccard_logo.png')
+logo_file = os.path.join(root_path, "bccard_logo.png")
 
-loading_txt = "AI가 분석에 맞는 코드를 생성중입니다. 분석을 하는 동안 잠시만 기다려주세요."
-dt_desc_txt = """
+LOADING_TXT = (
+    "AI가 분석에 맞는 코드를 생성중입니다. 분석을 하는 동안 잠시만 기다려주세요."
+)
+DT_DESC_TXT = """
             해당 그래프는 BC 카드의 내국인 소비 데이터를 기반으로 생성하였습니다.
             내국인 소비 데이터는 일자/시간/연령대별 전국의 가맹점 소비 내역을 포함하고 있습니다.
             자세한 사항은 아래 데이터 상품을 참고해주세요."""
 
-success_txt = "모델이 성공적으로 결과를 생성하였습니다."
-fail_txt = "파일 실행에 실패하였습니다."
+SUCCESS_TXT = "모델이 성공적으로 결과를 생성하였습니다."
+FAIL_TXT = "파일 실행에 실패하였습니다."
 
-async def simulate_typing_effect(text, typing_speed=.08):
+
+async def simulate_typing_effect(text, typing_speed=0.08):
     words = [word for word in text]
     placeholder = st.empty()
     output_text = " "
@@ -49,67 +51,84 @@ async def simulate_typing_effect(text, typing_speed=.08):
     for word in words:
         for letter in word:
             output_text += letter
-            placeholder.markdown(font_style + output_text + "</span>", unsafe_allow_html=True)
+            placeholder.markdown(
+                font_style + output_text + "</span>", unsafe_allow_html=True
+            )
             await asyncio.sleep(typing_speed)  # 비동기적으로 대기합니다.
 
         # output_text += ' '
-        placeholder.markdown(font_style + output_text + "</span>", unsafe_allow_html=True)
-    
+        placeholder.markdown(
+            font_style + output_text + "</span>", unsafe_allow_html=True
+        )
+
+
 def get_image(image_path):
-    with open(image_path, 'rb') as img_file:
-        return base64.b64encode(img_file.read()).decode('utf-8')
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode("utf-8")
+
 
 def init_page():
     st.set_page_config(
-        page_title='BC카드 금융빅데이터 플랫폼 검색엔진', 
-        page_icon= logo_file
-		)
-    
+        page_title="BC카드 금융빅데이터 플랫폼 검색엔진", page_icon=logo_file
+    )
+
     image_base64 = get_image(logo_file)
 
-    st.markdown(f"""
-    <img src="data:image/png;base64,{image_base64}" alt="Local Image" style="width: 50px; height: auto;"> 
+    st.markdown(
+        f"""
+    <img src="data:image/png;base64,{image_base64}" alt="Local Image" style="width: 50px; height: auto;">
     <span style="margin-left: 10px; font-size: 30px; font-weight: bold;">BC카드 금융빅데이터 플랫폼</span>
     <br><br>
-    """, unsafe_allow_html=True)
+    """,
+        unsafe_allow_html=True,
+    )
 
 
 async def main_ui():
     init_page()
-    
-    with st.form('question_answer'):
-        user_question = st.text_area("질문을 입력하세요:", key="user_question", label_visibility='visible')
+
+    with st.form("question_answer"):
+        user_question = st.text_area(
+            "질문을 입력하세요:", key="user_question", label_visibility="visible"
+        )
         with stylable_container(
-            'red',
+            "red",
             css_styles="""
             button {
                 background-color: #FF0000;
                 color: #F5F5F5
             }
-            """):
-            if st.form_submit_button(label='검색하기'):
+            """,
+        ):
+            if st.form_submit_button(label="검색하기"):
                 start_time = time.time()
-                if 'last_question' not in st.session_state or st.session_state.last_question != user_question:
+                if (
+                    "last_question" not in st.session_state
+                    or st.session_state.last_question != user_question
+                ):
                     st.session_state.last_question = user_question
                     # response = get_response(code_return, user_question, 'wizardcoder:34b-python')
-                    with st.spinner('AI가 답변을 생성중입니다..'):
-                        await simulate_typing_effect(loading_txt, typing_speed=0.1)
-                        response, graph_path, report_path = response_from_llm(user_question)
+                    with st.spinner("AI가 답변을 생성중입니다.."):
+                        await simulate_typing_effect(LOADING_TXT, typing_speed=0.1)
+                        response, graph_path, report_path = response_from_llm(
+                            user_question
+                        )
                         total_time = time.time() - start_time
-                        logger.info(f"소요 시간[모델 가동 X]:{total_time}")
-                        logger.info(f"response 결과:{response}")
-                                            
+                        logger.info("소요 시간[모델 가동 X]: %s", total_time)
+                        logger.info("response 결과: %s", response)
+
                     await simulate_typing_effect(response)
-                    if response == success_txt:
+                    if response == SUCCESS_TXT:
                         # st.image(logo_file, use_column_width=True)
                         st.image(graph_path, use_column_width=True)
-                        with open(report_path, 'r') as f:
-                            report = f.read()
+                        with open(report_path, "r", encoding="utf-8") as file:
+                            report = file.read()
                         await simulate_typing_effect(report)
                         total_time = time.time() - start_time
-                        logger.info(f"소요 시간[모델 가동 O]:{total_time}")
-                        await simulate_typing_effect(dt_desc_txt, typing_speed=0.1)
-                        st.markdown("""<table class="info-table" style="margin-left: auto; margin-right: auto;">
+                        logger.info("소요 시간[모델 가동 O]: %s", total_time)
+                        await simulate_typing_effect(DT_DESC_TXT, typing_speed=0.1)
+                        st.markdown(
+                            """<table class="info-table" style="margin-left: auto; margin-right: auto;">
                                 <br>
                                 </br>
                                 <tr>
@@ -125,12 +144,15 @@ async def main_ui():
                                     <td><a href="https://www.bigdata-finance.kr/dataset/datasetView.do?datastId=SET0300009" style="font-weight: bold;">바로가기</a></td>
                                 </tr>
                                 </table><br>
-                        """, unsafe_allow_html=True
+                        """,
+                            unsafe_allow_html=True,
                         )
-                    elif response == fail_txt:
+                    elif response == FAIL_TXT:
                         st.write("결과 호출에 실패하였습니다. 죄송합니다")
-                        # words = ["Hello,", "this", "is", "a", "simulation", "of", "ChatGPT", "typing."]
+                        # words = ["Hello,", "this", "is", "a",
+                        # "simulation", "of", "ChatGPT", "typing."]
                         # simulate_typing_effect(words, typing_speed=0.1)
+
 
 if __name__ == "__main__":
     asyncio.run(main_ui())
